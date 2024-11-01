f74efe8de89ab807a997b5d0c79576aa3cedf91a
Prince Asante <princeaffumasante@gmail.com>
2024-11-01 10:52:35 +0000

    build virtual keys of the keyboard

--- lib/word_huddle_page.dart
+++ lib/word_huddle_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:word_huddle_puzzle/huddle_provider.dart';
+import 'package:word_huddle_puzzle/keyboard_view.dart';
 import 'package:word_huddle_puzzle/wordle_view.dart';
 
 class WordHuddlePage extends StatefulWidget {
@@ -27,21 +28,26 @@ class _WordHuddlePageState extends State<WordHuddlePage> {
         child: Column(
           children: [
             Expanded(
-              child: Consumer<HuddleProvider>(
-                builder: (context, provider, child) => GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 5,
-                      mainAxisSpacing: 4,
-                      crossAxisSpacing: 4),
-                  itemCount: provider.hurdleBoard.length,
-                  itemBuilder: (context, index) {
-                    final wordle = provider.hurdleBoard[index];
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.70,
+                child: Consumer<HuddleProvider>(
+                  builder: (context, provider, child) => GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 5,
+                            mainAxisSpacing: 4,
+                            crossAxisSpacing: 4),
+                    itemCount: provider.hurdleBoard.length,
+                    itemBuilder: (context, index) {
+                      final wordle = provider.hurdleBoard[index];
 
-                    return WordleView(wordle: wordle);
-                  },
+                      return WordleView(wordle: wordle);
+                    },
+                  ),
                 ),
               ),
             ),
+            const KeyboardView(),
           ],
         ),
       ),
